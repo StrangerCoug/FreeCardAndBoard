5b04b2ed9997ec323437a45854f323251f7e91bc
StrangerCoug <strangercoug@hotmail.com>
2019-08-29 12:46:17 -0600

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.github.strangercoug.freecardandboard {
+	exports com.github.strangercoug.freecardandboard.objs;
+	exports com.github.strangercoug.freecardandboard.exceptions;
+	exports com.github.strangercoug.freecardandboard.enums;
+	exports com.github.strangercoug.freecardandboard.games.card;
+	exports com.github.strangercoug.freecardandboard.games.board;
+	exports com.github.strangercoug.freecardandboard;
+}
