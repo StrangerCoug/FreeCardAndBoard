305c9512b80198897cd0c08aa370e749c2270148
StrangerCoug <strangercoug@hotmail.com>
2019-11-12 11:53:30 -0700

    Add most of the methods from my old NinetyNine project Some of the methods had to be modified since players is an ArrayList instead of a promitive array.
    Signed-off-by: StrangerCoug <strangercoug@hotmail.com>

--- src/main/java/com/github/strangercoug/freecardandboard/games/card/NinetyNine.java
+++ src/main/java/com/github/strangercoug/freecardandboard/games/card/NinetyNine.java
@@ -29,6 +29,7 @@
 package com.github.strangercoug.freecardandboard.games.card;
 
 import com.github.strangercoug.freecardandboard.Player;
+import com.github.strangercoug.freecardandboard.enums.CardSuit;
 import com.github.strangercoug.freecardandboard.objs.Card;
 import com.github.strangercoug.freecardandboard.objs.Deck;
 import java.util.ArrayList;
@@ -39,6 +40,14 @@ import java.util.ArrayList;
  */
 public class NinetyNine extends CardGame {
 	int[] scores;
+	private CardSuit currentTrump; // for no-trump, this is null
+    private enum PremiumBid{DECLARE, REVEAL};
+    private PremiumBid premiumBid;
+    private Player premiumBidder;
+	
+	private enum PremiumBid {
+		DECLARE, REVEAL
+	}
 	
 	public NinetyNine() {
 		minPlayers = 3;
@@ -72,4 +81,123 @@ public class NinetyNine extends CardGame {
 		deck.shuffleDeck();
 	}
 	
+	/**
+     * Deals out the entire deck of cards to each player.
+     * 
+     * @param dealerPos the position of the current dealer
+     */
+    
+    public void dealCards(int dealerPos) {
+        deck.shuffleDeck();
+         
+        for (int i = dealerPos + 1; !deck.isEmpty(); i++) {
+            deck.dealCardTo(players.get(i % players.length));
+        }
+    }
+    
+    /**
+     * Determines which card won the trick.
+     * 
+     * @param cards  the cards played in order
+     * @param trump  the trump suit
+     * @return the highest trump if a card was played, the highest card of the
+     * suit led otherwise
+     */
+    
+    public Card findWinningCard(Card[] cards, CardSuit trump) {
+        boolean trumpFound = cards[0].getSuit().equals(trump);
+        int winningCardIndex = 0;
+        
+        for (int i = 1; i < cards.length; i++) {
+            if (trump != null) {
+                if (!trumpFound && cards[i].getSuit().equals(trump)) {
+                    trumpFound = true;
+                    winningCardIndex = i;
+                    continue;
+                }
+            }   
+                
+            if (!cards[i].getSuit().equals(cards[winningCardIndex].getSuit()))
+                continue;
+            
+            if (cards[i].outranks(cards[winningCardIndex]))
+                winningCardIndex = i;
+        }
+        
+        return cards[winningCardIndex];
+    }
+    
+    /**
+     * Tabulates the results of the hand. Currently does not support
+     * declarations or revelations.
+     */
+    public void scoreGame() {
+        boolean[] madeBid = new boolean[players.size()];
+        byte numWinners = 0;
+        byte winBonus;
+        
+        for (int i = 0; i < players.size(); i++) {
+            players.get(i).addToScore(players.get(i).getTricksWon());
+            madeBid[i] = players.get(i).getTricksWon() == players.get(i).getTricksBid();
+            
+            if (madeBid[i]) {
+                numWinners++;
+            }
+        }
+        
+        switch (numWinners) {
+            case 0: currentTrump = CardSuit.DIAMONDS;
+                break;
+            case 1: currentTrump = CardSuit.SPADES;
+                break;
+            case 2: currentTrump = CardSuit.HEARTS;
+                break;
+            case 3: currentTrump = CardSuit.CLUBS;
+                break;
+            default: currentTrump = null;
+        }
+        
+        if (numWinners == 0) {
+            return; // since there's no point in continuing
+        }
+        
+        if (players.size() == 5) {
+            winBonus = (byte) ((6 - numWinners) * 10);
+        }
+        
+        else {
+            if (numWinners == 4) {
+                winBonus = 0;
+            }
+            
+            else winBonus = (byte) ((4 - numWinners) * 10);
+        }
+        
+        for (int i = 0; i < players.size(); i++) {
+            if (madeBid[i]) {
+                players.get(i).addToScore(winBonus);
+            }
+        }
+    }
+    
+    public boolean isThereAWinner() {
+        for (Player player : players) {
+            if (player.getScore() >= 100) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public Player getFirstPlacePlayer() {
+        Player leader = players.get(0);
+        
+        for (int i = 1; i < players.size(); i++) {
+            if (players.get(i).getScore() > leader.getScore())
+                leader = players.get(i);
+        }
+        
+        return leader;
+    }
 }
