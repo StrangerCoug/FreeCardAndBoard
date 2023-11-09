94dcc43724626f0765674a5e600cf0116bd3b527
Jeffrey Hope <strangercoug@hotmail.com>
2023-11-09 16:51:41 -0600

    Remove redundand requires directive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.github.strangercoug.freecardandboard {
 	requires lombok;
-	requires java.logging;
 	exports com.github.strangercoug.freecardandboard.objs;
 	exports com.github.strangercoug.freecardandboard.exceptions;
 	exports com.github.strangercoug.freecardandboard.enums;
